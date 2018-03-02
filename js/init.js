dc7ce06019d915127a28879620f650ffffae5efb
DESKTOP-FO51F23\m <marcelorbianchi96@gmail.com>
2018-03-02 20:06:46 -0300

    Entrar

--- js/init.js
+++ js/init.js
@@ -1,4 +1,45 @@
+String.prototype.toHHMMSS = function () {
+  var sec_num = parseInt(this, 10); // don't forget the second parm
+  var hours = Math.floor(sec_num / 3600);
+  var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+  var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+  if (hours < 10) {
+      hours = "0" + hours;
+  }
+  if (minutes < 10) {
+      minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+      seconds = "0" + seconds;
+  }
+  var time = /*hours + ':' + */minutes + ':' + seconds;
+  return time;
+}
 window.reattach = {};
+window.reattach.timers = new Array()
+window.reattach.timer = function(index) {
+  if(typeof window.reattach.timers[index] === 'undefined') return;
+  var temp = window.reattach.timers[index].count.toHHMMSS();
+  window.reattach.timers[index].count = (parseInt(window.reattach.timers[index].count) - 1).toString();
+  $("#"+window.reattach.timers[index].selector).html(temp);
+  if (parseInt(window.reattach.timers[index].count) < 0) {
+    clearInterval(window.reattach.timers[index].interval);
+  window.reattach.timers[index].callback()
+  }
+}
+
+window.reattach.createCountdown = function(callback,seconds) {
+  window.reattach.timers.push({
+    selector: 'timer-'+Math.round(+new Date()/1000) + new Date().getTime(),
+    count: seconds.toString(),
+    interval: setInterval(window.reattach.timer, 1000, window.reattach.timers.length),
+    callback: callback
+  })
+  return '<span id="'+window.reattach.timers.slice(-1).pop().selector+'"></span>'
+}
+
+
 window.reattach.logReset = function(){
   $(".log").toggle("slow");
   $(".log").html("");
@@ -10,49 +51,141 @@ window.reattach.logAdd = function(msg){
 window.reattach.start = function(){
   window.reattach.logReset()
   window.reattach.connect()
-    .then(()=>{return window.reattach.getTransactionObjects(window.reattach.data.input,'bundles')})//assumed input is bundle, search txs
-    .then( (transactionObjects) => {
-      if(transactionObjects.length > 0)
-        return Promise.resolve(transactionObjects)//fine, we have the bundle
-      else {
-        return window.reattach.getBundleHashFromTransactionHash(window.reattach.data.input)//assumed input is transaction, search bundle hash
-          .then( (bundleHash) => {
-            if(bundleHash)
-              return window.reattach.getTransactionObjects(bundleHash,'bundles')//fine, we have bundle hash, search txs
-            else
-              return Promise.reject('address')//input isn't bundle hash, isn't tx hash, is address
+    .then(()=>{
+      if(window.reattach.data.input.length === 90)
+        return window.reattach.getTransactionObjects(window.iota.utils.noChecksum(window.reattach.data.input),'addresses')
+          .then((transactionObjects)=>{
+            return Promise.resolve(['address',transactionObjects])
           })
-          .catch((error) => {
-            if(error==='address')
-              window.reattach.error('Invalid hash, you can find your transaction on <a target="_blank" href="https://thetangle.org/address/'+window.reattach.input+'">here</a>'+error)
-            else
-              window.reattach.error(error)
+          .catch((error)=>{return Promise.reject(error)})
+      else if(window.reattach.data.input.substr(window.reattach.data.input.length - 3) === '999')
+        return window.reattach.getBundleHashFromTransactionHash(window.reattach.data.input)
+          .then((bundleHash)=>{
+            return window.reattach.getTransactionObjects(bundleHash,'bundles')
+              .then((transactionObjects)=>{
+                return Promise.resolve(['transaction',transactionObjects])
+              })
+              .catch((error)=>{Promise.reject})//error
           })
+          .catch((error)=>{return Promise.reject(error)})//error
+      else
+        return window.reattach.getTransactionObjects(window.reattach.data.input,'addresses')
+          .then( (transactionObjects) => {
+            if(transactionObjects.length)
+              return Promise.resolve(['address',transactionObjects])
+            else
+              return window.reattach.getTransactionObjects(window.reattach.data.input,'bundles')
+                .then((transactionObjects)=>{
+                  return Promise.resolve(['bundle',transactionObjects])
+                })
+                .catch((error)=>{return Promise.reject(error)})//isnt address, transaction or bundle
+          })
+          .catch((error)=>{return Promise.reject(error)})//error with the node
+    })
+    .then((data)=>{
+      var inputType = data[0],
+          transactionObjects = data[1]
+      if(inputType==='address'){
+        return Promise.reject('address')
+      }else{
+        //normal progress
+        return Promise.resolve(transactionObjects)
       }
     })
     .then( (transactionObjects) => {
           window.reattach.data.bundle = transactionObjects
-          window.reattach.logAdd('<h5>Bundle</h5> '+transactionObjects[0].bundle)
+          window.reattach.logAdd('<h5>Bundle</h5> <a target="_blank" href="https://thetangle.org/bundle/'+transactionObjects[0].bundle+'">'+transactionObjects[0].bundle+'</a>')
           return window.reattach.getStatus(transactionObjects)
     })
-    .then()
+    .then( (status) => {
+      if(status.length) {
+        window.reattach.logAdd('<h6 class="status confirmed">Confirmed</h6>')
+        return Promise.reject('confirmed')
+      } else {
+        window.reattach.logAdd('<h6 class="status unconfirmed">Unconfirmed - '+window.reattach.createCountdown(function(){alert('hola')},60)+'</h6><hr>')
+        
+        return window.reattach.doReattach()
+      }
+    })
+    .then( (success) => {
+      if(success) {
+        window.reattach.logAdd('<h6>Reattached</h6> <a target="_blank" href="https://thetangle.org/transaction/'+success[0].hash+'">'+success[0].hash+'</a>')
+        window.reattach.data.tailObjects.push(success[0])
+        return window.reattach.doPromote(success[0].hash)
+      }else{
+        return window.reattach.doPromote()
+      }
+    })
+    .then( (success) => {
+      if(success) {
+        window.reattach.logAdd('')
+      }else{
+        window.reattach.logAdd('Verifying on..2')
+      }
+    })
     .catch((error) => {
-      if(error==='address')
-        window.reattach.error('Invalid hash, you can find your transaction on <a target="_blank" href="https://thetangle.org/address/'+window.reattach.input+'">here</a>'+error)
-      else
-        window.reattach.error(error)
+      switch(error) {
+        case 'confirmed':
+          window.reattach.error()
+        break;
+        case 'address':
+          window.reattach.error('You entered an <strong>address</strong>, can find your bundle or transaction hash on a tangle explorer like <a target="_blank" href="https://thetangle.org/address/'+window.reattach.data.input+'">that</a>')
+        break;
+        case 'Error: Request Error: Invalid bundles input':
+          window.reattach.error('Invalid transaction')
+        break;
+        default:
+          window.reattach.error(error)
+        break;
+      }
     })
 }
 
 window.reattach.doReattach = function(bundle) {
-
+  if(window.reattach.data.reattach === false)
+    return Promise.resolve(false)
+  else
+    return new Promise( (resolve,reject) => {
+      window.iota.api.replayBundle(window.reattach.data.tailObjects[0].hash, 3, 14, (error,success) => {
+        if(error) reject(error)
+        else resolve(success)
+      })
+    })
 }
+
+window.reattach.doPromote = function(tailHash) {
+  if(!window.reattach.data.promote)
+    return Promise.resolve(false)
+  if(typeof tailHash === 'undefined')
+    var tailHash = window.reattach.data.input()
+  return new Promise( (resolve,reject) => {
+    var count = 0
+    window.reattach.data.promoteID = Math.round(+new Date()/1000) + new Date().getTime()
+    window.reattach.logAdd('<h6>Promoted <span id="counter-'+window.reattach.data.promoteID+'">0</span>/'+(window.reattach.data.tailObjects[0].lastIndex+1)+'</h6>')
+    window.iota.api.promoteTransaction(tailHash, 3, 14, [{
+      address: 'REATTACH9DOT9ONLINE99999999999999999999999999999999999999999999999999999999999MRB',
+      tag: 'REATTACHDOTONLINE',
+      message: window.iota.utils.toTrytes('http://IOTAReatta.ch - IOTA Reattacher/Promoter'),
+      value: 0
+    }],{
+      delay:1000,
+      interrupt: () => {
+          $('#counter-'+window.reattach.data.promoteID).text(++count)
+          return count >= (window.reattach.data.tailObjects[0].lastIndex+1);
+      }
+    }, (error,result) => {
+      if(error) reject(error)
+      else resolve(true)
+    })
+  })
+}
+
 window.reattach.getStatus = function(bundle) {
-  var tails = bundle.filter((transactionObject)=>{
+  window.reattach.data.tailObjects = bundle.filter((transactionObject)=>{
     return !transactionObject.currentIndex
   })
-  var hashes = bundle.map((transactionObject)=>{
-    return transactionObject.hash
+  var hashes = window.reattach.data.tailObjects.map((tailObject)=>{
+    return tailObject.hash
   })
   return new Promise( (resolve,reject) => {
     window.iota.api.getLatestInclusion(hashes,(error,states) => {
@@ -95,13 +228,9 @@ window.reattach.getTransactionObjects = function (input,inputType) {
 
 window.reattach.getBundleHashFromTransactionHash = function (input) {
   return new Promise( (resolve,reject) => {
-    window.iota.api.getTransactionsObjects([input], (error,transactionObjects)=>{
+    window.iota.api.getTransactionsObjects([input], (error,transactionsObject)=>{
       if(error) reject(error)
-      else 
-        if(transactionObjects.length > 0)
-          resolve(transactionObjects[0].bundle)
-        else
-          resolve(false)
+      else resolve(transactionsObject[0].bundle)
     })
   })
 };
@@ -121,7 +250,8 @@ window.reattach.toggleInputDisables = function () {
 };
 
 window.reattach.error = function(msg){
-  window.reattach.logAdd(msg)
+  if(typeof msg !== 'undefined')
+    window.reattach.logAdd(msg)
   window.reattach.toggleInputDisables()
 };
 
